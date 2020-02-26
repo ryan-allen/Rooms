11cd27ea6151f86602175492e8cfd0c5adecbf17
Ryan Allen <ryan@137.ms>
2020-02-26 21:35:34 +0000

    story telling through object trees

--- Program.cs
+++ Program.cs
@@ -1,34 +1,81 @@
 using System;
+using System.Collections.Generic;
 
 namespace Rooms
 {
-
     class Generator
     {
-        public Game Generate() => new Game();
+        public Game Generate()
+        {
+            return new Game
+            {
+                Player = new Player
+                {
+                    HitPoints = 8,
+                    Strength = 5
+                },
+                CurrentRoom = new Room
+                {
+                    Description = "You're in a small closet.",
+                    Exits = new List<Exit> {
+                        new Exit {
+                            Description = "There is a door that leads out of the closet.",
+                            Option = "Take the door and leave the closet.",
+                            Room = new Room {
+                                Description = "You find yourself in a decrepit bedroom.",
+                                Monsters = new List<Monster> {
+                                    new Monster {
+                                        Name = "Zombie Kid",
+                                        Description = "A once sweet kid who's clearly turned into a murderous Zombie.",
+                                        Strength = 3,
+                                        HitPoints = 3
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+        }
     }
 
     class Game
     {
+        public Room CurrentRoom { get; set; }
+        public Player Player { get; set; }
+
         public void Play()
         {
             Console.WriteLine("You win!");
         }
     }
 
+    class Exit
+    {
+        public string Description { get; set; }
+        public string Option { get; set; }
+        public Room Room { get; set; }
+    }
+
     class Room
     {
-
+        public string Description { get; set; }
+        public List<Monster> Monsters { get; set; } = new List<Monster>();
+        public List<Exit> Exits { get; set; } = new List<Exit>();
     }
 
     class Player
     {
-
+        public int HitPoints { get; set; }
+        public int Strength { get; set; }
     }
 
     class Monster
     {
-
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int HitPoints { get; set; }
+        public int Strength { get; set; }
     }
 
     class Program

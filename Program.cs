7681e19d007f81fbe66e42b48aa8eb5ec7e9c30d
Ryan Allen <ryan@137.ms>
2020-02-26 01:37:52 +0000

    console app boilerplate

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rooms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
